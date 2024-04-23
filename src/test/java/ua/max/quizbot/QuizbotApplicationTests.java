7a89729774c3e7275433d2a084d51d4029708d38
maximpavliv <maxim.pavliv@gmail.com>
2024-04-23 09:07:15 +0200

    Simple Rest API printing hello

---
+++ src/test/java/ua/max/quizbot/QuizbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.max.quizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
