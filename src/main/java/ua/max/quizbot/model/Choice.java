f3e97bce6e715e4fb0d66d7429fdb94c6d43c26a
maximpavliv <maxim.pavliv@gmail.com>
2024-05-29 19:20:10 +0200

    Use lombok to remove boilerplate code

--- src/main/java/ua/max/quizbot/model/Choice.java
+++ src/main/java/ua/max/quizbot/model/Choice.java
@@ -1,8 +1,12 @@
 package ua.max.quizbot.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Choice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,27 +17,4 @@ public class Choice {
     @ManyToOne
     @JoinColumn(name = "questionId", referencedColumnName = "questionId")
     private Question question;
-
-    public Long getChoiceId() {
-        return choiceId;
-    }
-
-    public void setChoiceId(Long choiceId) {
-        this.choiceId = choiceId;
-    }
-
-    public String getChoiceText() {
-        return choiceText;
-    }
-
-    public void setChoiceText(String choiceText) {
-        this.choiceText = choiceText;
-    }
-    public Question getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
 }
--- src/main/java/ua/max/quizbot/model/QuizExercise.java
+++ src/main/java/ua/max/quizbot/model/QuizExercise.java
@@ -1,10 +1,14 @@
 package ua.max.quizbot.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class QuizExercise {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,52 +31,4 @@ public class QuizExercise {
     @OneToOne
     @JoinColumn(name = "userQuizExerciseChoiceId", referencedColumnName = "quizExerciseChoiceId")
     private QuizExerciseChoice userQuizExerciseChoice;
-
-    public Long getQuizExerciseId() {
-        return quizExerciseId;
-    }
-
-    public void setQuizExerciseId(Long quizExerciseId) {
-        this.quizExerciseId = quizExerciseId;
-    }
-
-    public Quiz getQuiz() {
-        return quiz;
-    }
-
-    public void setQuiz(Quiz quiz) {
-        this.quiz = quiz;
-    }
-
-    public Question getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(Question question) {
-        this.question = question;
-    }
-
-    public List<QuizExerciseChoice> getQuizExerciseChoices() {
-        return quizExerciseChoices;
-    }
-
-    public void setQuizExerciseChoices(List<QuizExerciseChoice> quizExerciseChoices) {
-        this.quizExerciseChoices = quizExerciseChoices;
-    }
-
-    public Integer getIdx() {
-        return idx;
-    }
-
-    public void setIdx(Integer idx) {
-        this.idx = idx;
-    }
-
-    public QuizExerciseChoice getUserQuizExerciseChoice() {
-        return userQuizExerciseChoice;
-    }
-
-    public void setUserQuizExerciseChoice(QuizExerciseChoice userQuizExerciseChoice) {
-        this.userQuizExerciseChoice = userQuizExerciseChoice;
-    }
 }
